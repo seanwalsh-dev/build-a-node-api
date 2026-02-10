667a7c448e846710736f59428c5b76676701d77d
seanwalsh <contact.seanwalsh.dev@gmail.com>
2026-02-10 10:12:52 -0700

    adding content-type

---
+++ index.js
@@ -0,0 +1,20 @@
+import http from 'node:http'
+import { getDataFromDB } from './database/db.js'
+
+const PORT = 8000
+
+
+
+const server = http.createServer(async (req, res) => {
+  
+  const destinations = await getDataFromDB()
+  
+  if (req.url === '/api' && req.method === 'GET'){
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 200
+    res.end(JSON.stringify(destinations), () => console.log('get /api requested'))
+  }
+  
+})
+
+server.listen(PORT, () => console.log(`server running on port: ${PORT}`))
