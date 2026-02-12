cbfc8d4faf2bb9e74ec05500255754f5840bba9d
seanwalsh <contact.seanwalsh.dev@gmail.com>
2026-02-12 16:26:55 -0700

    filter by query params - attempt 2'

--- index.js
+++ index.js
@@ -21,45 +21,82 @@ const server = http.createServer(async (req, res) => {
     const filteredDestinations = destinations
     sendJSONResponse (res, 200, filteredDestinations, 'GET /api requested')
 
-  }else if (queryObj.continent && req.method === 'GET') {
-    // ***  WORKING HERE  ***
-    console.log('queryObj.continent: ', queryObj.continent)
-    const locationType = 'continent'
-    const locationName = queryObj.continent
-    const filteredDestinations = getDataByQueryParams(locationType, locationName, queryObj, destinations)
-    sendJSONResponse (res, 200, filteredDestinations, 'GET /api?continent requested')
+  }else {
 
-    console.log('filteredDestinations: ', filteredDestinations)
+    //  *** MOVE TO getDataByQueryParams.js ***
 
-  }else if (queryObj.country && req.method === 'GET') {
-    console.log('queryObj.country: ', queryObj.country)
-    const locationType = 'country'
-    const locationName = queryObj.country
-    const filteredDestinations = getDataByQueryParams(locationType, locationName, queryObj, destinations)
-    sendJSONResponse (res, 200, filteredDestinations, 'GET /api?continent requested')
+    const continent = queryObj.continent
+    const country = queryObj.country
+    const isOpen = queryObj.is_open_to_public
 
+    let filteredDestinations = destinations
+
+    if(continent){
+      filteredDestinations = filteredDestinations.filter((destination) => 
+        destination.continent.toLowerCase() === continent.toLowerCase()
+      )
+    }
+
+    if(country){
+      filteredDestinations = filteredDestinations.filter((destination) => 
+        destination.country.toLowerCase() === country.toLowerCase()
+      )
+    }
+
+    if(isOpen){
+      filteredDestinations = filteredDestinations.filter((destination) => 
+        destination.is_open_to_public.toString().toLowerCase() === isOpen.toLowerCase()
+      )
+      console.log('isOpen: ', isOpen.toLowerCase())
+    }
+
+    if(filteredDestinations.length === 0){
+      filteredDestinations = {error: "not found", message: "The requested route does not exist"}
+    }
+
+    sendJSONResponse (res, 200, filteredDestinations, 'GET /api requested')
   }
-  
-  // if (urlObj.pathname === '/api' && req.method === 'GET'){
-  //   let filteredDestinations = destinations
-  //   sendJSONResponse (res, 200, filteredDestinations, 'GET /api requested')
 
-  // }else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
-  //   const continent = req.url.split('/').pop()
-  //   const filteredData = getDataByPathParams (destinations, 'continent', continent)
-  //   sendJSONResponse (res, 200, filteredData, 'GET /api/continent requested')
-
-  // }else if (req.url.startsWith('/api/country') && req.method === 'GET') {
-  //   const country = req.url.split('/').pop()
-  //   const filteredData =  getDataByPathParams (destinations, 'country', country)
-  //   sendJSONResponse(res, 200, filteredData, 'GET /api/country requested')
-
-  // }else {
-  //   const errObj = {error: "not found", message: "The requested route does not exist"}
-  //   sendJSONResponse (res, 404, errObj, 'request not found')
-  
-  // }
+  //  *** MOVE TO getDataByQueryParams.js ***
   
 })
 
 server.listen(PORT, () => console.log(`server running on port: ${PORT}`))
+
+
+
+
+
+
+
+
+
+
+
+
+  // if (urlObj.search === '' && req.method === 'GET') {
+  //   console.log('urlObj.pathname: ', urlObj.pathname)
+  //   const filteredDestinations = destinations
+  //   sendJSONResponse (res, 200, filteredDestinations, 'GET /api requested')
+
+  // }else if (queryObj.continent && req.method === 'GET') {
+  //   // ***  WORKING HERE  ***
+  //   console.log('queryObj.continent: ', queryObj.continent)
+  //   const locationType = 'continent'
+  //   const locationName = queryObj.continent
+  //   const filteredDestinations = getDataByQueryParams(locationType, locationName, queryObj, destinations)
+  //   sendJSONResponse (res, 200, filteredDestinations, 'GET /api?continent requested')
+
+  //   console.log('filteredDestinations: ', filteredDestinations)
+
+  // }else if (queryObj.country && req.method === 'GET') {
+  //   console.log('queryObj.country: ', queryObj.country)
+  //   const locationType = 'country'
+  //   const locationName = queryObj.country
+  //   const filteredDestinations = getDataByQueryParams(locationType, locationName, queryObj, destinations)
+  //   sendJSONResponse (res, 200, filteredDestinations, 'GET /api?continent requested')
+
+  // }else {
+  //   const errObj = {error: "not found", message: "The requested route does not exist"}
+  //   sendJSONResponse (res, 404, errObj, 'request not found')
+  // }
--- utils/getDataByQueryParams.js
+++ utils/getDataByQueryParams.js
@@ -12,3 +12,29 @@ export function getDataByQueryParams(locationType, locationName, queryObj, desti
         }
       })
 }
+
+
+
+// export function getDataByQueryParams(locationType, locationName, queryObj, destinations) {
+//   const isOpen = queryObj.is_open_to_public
+  
+//       return destinations.filter((destination) =>{
+//         if(isOpen){
+//           return destination[locationType].toLowerCase() === locationName.toLowerCase() 
+//           &&
+//           destination.is_open_to_public.toString().toLowerCase() === isOpen.toLowerCase()
+  
+//         }else{
+//           return destination[locationType].toLowerCase() === locationName.toLowerCase()
+//         }
+//       })
+// }
+
+//  Try adjusting your getDataByQueryParams
+//  to accept the full data set and query object,
+//  and handle all three filters
+//  country, continent, is_open_to_public, together
+
+//  your filter only works when a continent or country is provided.
+//  How can you handle any combination of the three query
+//  params, including just is_open_to_public
