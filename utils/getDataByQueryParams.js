e468d2bec4a5463caeff7cacb9fed0993e19197e
seanwalsh <contact.seanwalsh.dev@gmail.com>
2026-02-12 20:56:50 -0700

    filter by query parameters - fixed string to boolean

--- utils/getDataByQueryParams.js
+++ utils/getDataByQueryParams.js
@@ -20,36 +20,10 @@ export function getDataByQueryParams(queryObj, destinations) {
 
   if(isOpen){
     filteredDestinations = filteredDestinations.filter((destination) => 
-      destination.is_open_to_public.toString().toLowerCase() === isOpen.toLowerCase()
+      destination.is_open_to_public === JSON.parse(isOpen.toLowerCase())
     )
     console.log('isOpen: ', isOpen.toLowerCase())
   }
 
   return filteredDestinations
 }
-
-
-
-// export function getDataByQueryParams(locationType, locationName, queryObj, destinations) {
-//   const isOpen = queryObj.is_open_to_public
-  
-//       return destinations.filter((destination) =>{
-//         if(isOpen){
-//           return destination[locationType].toLowerCase() === locationName.toLowerCase() 
-//           &&
-//           destination.is_open_to_public.toString().toLowerCase() === isOpen.toLowerCase()
-  
-//         }else{
-//           return destination[locationType].toLowerCase() === locationName.toLowerCase()
-//         }
-//       })
-// }
-
-//  Try adjusting your getDataByQueryParams
-//  to accept the full data set and query object,
-//  and handle all three filters
-//  country, continent, is_open_to_public, together
-
-//  your filter only works when a continent or country is provided.
-//  How can you handle any combination of the three query
-//  params, including just is_open_to_public
