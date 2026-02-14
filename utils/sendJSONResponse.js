578be85e6efd725fd9665cafd8aa8a792ef452aa
seanwalsh <contact.seanwalsh.dev@gmail.com>
2026-02-14 10:37:38 -0700

    CORS - added access controls

--- utils/sendJSONResponse.js
+++ utils/sendJSONResponse.js
@@ -1,5 +1,7 @@
 export function sendJSONResponse (res, statusCode, data, log) {
   res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET')
   res.statusCode = statusCode
   res.end(JSON.stringify(data), () => console.log(log))
 }
